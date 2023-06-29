0b3c5f371b83ab6edf56188eb6a9aa49f964b626
RNMUDS <ryonaka@musashino-u.ac.jp>
2023-06-29 12:48:48 +0900

    modify python code

--- MD2/GAS/README_GAS.md
+++
@@ -1,9 +0,0 @@
-### GAS
-- Google App Scriptを用いたラズパイ＋センサの測定データのアップロード
-- Google SpreadSheetに追記されていく
-- コード
-  - data_up.py：シンプルなアップロードテスト
-  - ultra_sonic.py：超音波センサによる距離データの取得
-  - ultra_sonic_async.py：シートへの書き込み時間を考慮した距離データの自動追記
-  - ultra_sonic_pc.py：PCで疑似データを定期アップロードテスト
-  - ultra_sonic_pc_async.py：シートへの書き込み時間を考慮した距離データの自動追記
--- MD2/GAS/ultra_sonic_raspi.py
+++ MD2/GAS/ultra_sonic_raspi.py
@@ -1,14 +1,40 @@
+import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import requests
 
 upload_url = "自分のURLを貼り付け"
 
+TRIG = 16
+ECHO = 18
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+    print("setup OK")
+
+def distance():
+    GPIO.output(TRIG, 0)
+    time.sleep(0.000002)
+    GPIO.output(TRIG, 1)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, 0)
+
+    while GPIO.input(ECHO) == 0:
+        a = 0
+    time1 = time.time()
+    while GPIO.input(ECHO) == 1:
+        a = 1
+    time2 = time.time()
+    during = time2 - time1
+    return during * 340 / 2 * 100
+
 def loop():
     print("start")
     while True:
         try:
-            dis = 100
+            dis = distance()
             now = datetime.now()
             dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
             upload_data = dt_string + ', Distance: %.2f' % dis
@@ -19,8 +45,13 @@ def loop():
         except Exception as e:
             print("An error occurred: ", str(e))
 
+def destroy():
+    GPIO.cleanup()
+
 if __name__ == "__main__":
+    setup()
     try:
         loop()
     except KeyboardInterrupt:
+        destroy()
         print("\nProgram stopped by user")
